277e9b76650096b5b8aa85b3db074e870bb77d48
ngochieu642 <ngochieu10041997@gmail.com>
2019-04-05 11:29:20 +0700

    Delete unused files

--- ARM_F1_Periph/User/color.h
+++
@@ -1,5 +0,0 @@
-#include "stm32f10x.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-void SetColorWithCode(int channel, uint64_t colorCode);
--- ARM_F1_Periph/User/colorRun.h
+++
@@ -1,7 +0,0 @@
-#include "stm32f10x.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include "color.h"
-#include "sparkle.h"
-
-void ColorRun(uint64_t BackGround, uint64_t RunningColor, uint16_t frequency);
